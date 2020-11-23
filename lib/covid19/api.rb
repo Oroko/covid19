28fcb1bd0257c11dc0a3a0212df9007bfcdfb96f
Oroko <aoroko09@gmail.com>
2020-11-23 16:14:41 -0500

    add the functionality data is fetched only if the continent data is not already in the array

--- lib/covid19/api.rb
+++ lib/covid19/api.rb
@@ -6,12 +6,13 @@ module Covid19
       url = 'https://disease.sh/v3/covid-19/continents?yesterday=true'
       response = HTTParty.get(url)
       response.each do |item|
-        unless item['continent'].nil?
-          Covid19::Continent.new(item)
-          
-        end
-        #binding.pry
+        unless  Covid19::Continent.nil?
+
+        Covid19::Continent.new(item)
+
+        # binding.pry
       end
-    end
+   end
   end
+end
 end
