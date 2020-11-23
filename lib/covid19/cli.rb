2aa008d771db572bf3bdd74fb34f25b428e1f83c
Oroko <aoroko09@gmail.com>
2020-11-22 19:45:09 -0500

    add functionality choose a continent by number and get more information back from the api for the chosen continent

--- lib/covid19/cli.rb
+++ lib/covid19/cli.rb
@@ -4,20 +4,63 @@ module Covid19
       puts 'Welcome to the Covid19 CLI App'
       puts '______________________________'
       puts ''
-      puts 'Here is a list of Continents enter a number to choose a "Continent", or "exit" to Exit the app'
+      puts 'Here is a list of Continents'
       puts ''
       Covid19::API.fetch_continent_data
 
       continent_list
+      prompt
+
+     
+      chosen_continent
+      
     end
 
-    def continent_list
-      continents = Covid19::Continent.all
-      continents.each.with_index(1) do |c, i|
-        puts "#{i}. #{c.continent}"
+    def continents
+      continents = Covid19::Continent.all.collect do |c|
+        c.continent
       end
       #binding.pry
       
+    end
+    
+    
+
+    def continent_list 
+      continents.each.with_index(1) do |c, i|
+        puts "#{i}. #{c}"
+      end
+    end
+    
+    def chosen_continent
+      continent = gets.strip.to_i
+      
+      puts "You have chosen #{continents[continent - 1]}"
+      if continent.between?(1, continents.length)
+        puts "#{continents[continent - 1]} has:"
+        puts ''
+         continent_data(continent)
+       
+      else
+        puts 'Enter a number between 1 - 6'
+      end
+
+    end
+
+    def continent_data(continent)
+      cont = Covid19::Continent.find_by_continent(continents[continent - 1]) 
+      cont.each do |c|
+        puts "Total Cases #{c.cases}"
+        puts "Total deaths #{c.deaths}"
+      end
+     # binding.pry
+      #continents[continent -1]
+      #binding.pry
+    end
+
+    def prompt
+      puts ''
+      puts 'Enter a number to choose a "Continent" or "exit" to Exit the app'
     end
   end
 end
